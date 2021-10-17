41ed15ce358ecb07df0359adcfad42fc2595ffef
pranavkolle <pranavkolle@gmail.com>
2021-10-18 00:34:26 +0530

    test case 1
    test case 1

---
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,40 @@
+package calculator;
+
+import java.util.StringTokenizer;
+
+class StringCalculator {
+
+	public int add(String input) 
+  {
+  	int a = 0; int b = 0; int sum = 0;
+  	StringTokenizer st = new StringTokenizer(input,",");
+  	if(st.hasMoreTokens())
+  	{
+  		String temp = st.nextToken();
+  		try
+  		{
+  			a = Integer.parseInt(temp);
+  		}
+  		catch(Exception e)
+  		{
+  			a = 0;
+  		}
+  	}
+  	if(st.hasMoreTokens())
+  	{
+  		String temp = st.nextToken();
+  		try
+  		{
+  			b = Integer.parseInt(temp);
+  		}
+  		catch(Exception e)
+  		{
+ 			b = 0;
+  		}
+  	}
+  	sum = a + b;
+//  	System.out.println(input+"  "+sum);
+      return sum;
+  }
+
+}
